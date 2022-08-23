981e761e63f70977bac73629aa3e4d15496988ff
Hubol <hubol.gordon@gmail.com>
2022-08-23 18:42:22 -0500

    More messing with this. Very impressive

--- pages/api/media/[...slug].ts
+++ pages/api/media/[...slug].ts
@@ -2,9 +2,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import send from "send";
 import appRootPath from "app-root-path";
+import * as fs from "fs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const name = req.url!.substring("/api/media/".length);
+    console.log(fs.readdirSync("./"));
+    console.log(fs.readdirSync(appRootPath as any));
     console.log(name);
     send(req, name, { root: `${appRootPath}/media` }).pipe(res);
 }
