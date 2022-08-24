028e7ca7904eac0db3d88d056ab93e65445e3185
Hubol <hubol.gordon@gmail.com>
2022-08-23 19:15:24 -0500

    The vercel UI lied to me haha

--- components/getMediiiaPath.ts
+++ components/getMediiiaPath.ts
@@ -1,6 +1,6 @@
 import { path } from "app-root-path";
 
 export function getMediiiaPath() {
-    if (process.env.NODE_ENV === "production") return "./_next/static/mediiia";
+    if (process.env.NODE_ENV === "production") return "./.next/static/mediiia";
     return `${path}/media`;
 }
